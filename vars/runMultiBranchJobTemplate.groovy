59f71acbb27772adfbb174b0481602dc172da283
shashank-ssriva <shashank.ssriv@gmail.com>
2020-04-09 13:39:18 +0530

    Modify the template

--- vars/runMultiBranchJobTemplate.groovy
+++ vars/runMultiBranchJobTemplate.groovy
@@ -2,17 +2,21 @@ def call(Map config = [:]) {
 
     pipeline {
         agent any
+
         stages {
-    
             stage('Build Java src') {
                 steps {
+                    script {
                     runMavenBuild
+                    }
                 }
             }
 
             stage('Build Anugular src') {
                 steps {
+                    script {
                     runAngularBuild
+                    }
                 }
             }
         }
