0cfe6ec0038f16c25aa7fcb803cb07b1b7c016d6
Shashank Srivastava <shashank.ssriv@outlook.com>
2020-04-13 22:31:34 +0530

    Use other variable in shell script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "Building Angular source code..."
 echo "This script will execute NPM to build the Angular UI application."
-echo ${environmentId}
+echo "This is the Environment to use ${environmentId}"
